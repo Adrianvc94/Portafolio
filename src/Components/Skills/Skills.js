af33257e99e63c8d557444bffa641910f3261a0f
Adrianvc94 <avicamleon@gmail.com>
2021-09-24 13:47:01 -0600

    The Skills part is complete, but I might do some changes in the future

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -4,7 +4,13 @@ import Line from "../../Images/Line1.png";
 
 import HTML from "../../Images/HTML_Logo.png";
 import CSS from "../../Images/CSS_Logo.png";
+
 import JS from "../../Images/JS_Logo.png";
+import CSharp from "../../Images/CSharp_Logo.png";
+import SQL from "../../Images/SQL_Logo.png";
+import React from "../../Images/React_Logo.png";
+import DotNet from "../../Images/dotnet.png";
+
 import Line2 from "../../Images/Line2.png";
 
 const Skills = () => {
@@ -14,7 +20,9 @@ const Skills = () => {
   // C#
   // SQL
   // React
-  // 
+  // .Net
+  // Bootstrap
+  // Xamarin
 
   return (
     <section className={styles.container}>
@@ -22,7 +30,7 @@ const Skills = () => {
       <div className={styles.title_container}>
         <p className={styles.etiquetaP}>{etiquetaH2Apertura}</p>
         <h2 className={styles.skills}>
-          Skills
+          Skills Set
           <img className={styles.image} src={Line} alt="Decorative line" />
         </h2>
         <p className={styles.etiquetaP}>{etiquetaH2Cierre}</p>
@@ -33,10 +41,11 @@ const Skills = () => {
         <Skill image={CSS} />
         <Skill image={JS} />
 
-        <Skill image={HTML} />
-        <Skill image={CSS} />
-        <Skill image={JS} />
-        <Skill image={JS} />
+        <Skill image={CSharp} />
+        <Skill image={SQL} />
+        <Skill image={React} />
+        <Skill image={DotNet} />
+        
         
       </div>
     </section>
