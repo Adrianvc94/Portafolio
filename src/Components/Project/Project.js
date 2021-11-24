d9468df816dc779630fbb923bc14ba843cbc6aa4
Adrianvc94 <avicamleon@gmail.com>
2021-11-24 09:02:14 -0600

    Adding movie project

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -11,22 +11,28 @@ const Project = (props) => {
           <div className={styles.images_container}>
             <img src={folder} alt="Folder" />
             <div>
-              <a href={props.github}>
+              <a href={props.github} target="_blank" rel="noopener noreferrer">
                 <img
                   className={styles.github_link}
                   src={github}
                   alt="Github of the project"
                 />
               </a>
-
-              <img
-                className={styles.github_link}
-                alt="Link for the project"
-                src={link}
-              />
+              <a href={props.link} target="_blank" rel="noopener noreferrer">
+                <img
+                  className={styles.github_link}
+                  alt="Link for the project"
+                  src={link}
+                />
+              </a>
             </div>
           </div>
           <h5 className={`card-title ${styles.title}`}>{props.title}</h5>
+          <img
+            className={`card-img ${styles.projectImg}`}
+            src={props.projectImg}
+            alt="Demo"
+          />
           <p className="card-text">{props.description}</p>
           <p className="card-text">{props.tecnologies}</p>
         </div>
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,6 @@
 import styles from "./Projects.module.css";
 import Line from "../../Images/Line1.png";
+import movie_app from "../../Images/movie-app.png";
 
 import Project from "../Project/Project";
 
@@ -8,6 +9,8 @@ const Projects = () => {
   var etiquetaH2Cierre = "</h2>";
 
   var description = "Vestibulum laoreet ipsum in elementum dignissim. Aliquam erat volutpat. Class aptent taciti sociosqu ad litora orquent per conubia nostra";
+
+  var MovieList_Description = "The project makes use of TMDb (The Movie Database) API to search for movie and TV show data.";
 
   return (
     <section id="projects" className={`container ${styles.main_container}`}>
@@ -23,8 +26,8 @@ const Projects = () => {
 
       <div className={`row g-4 ${styles.grid}`}>
 
-        <Project github="www.github.com" link="example.com" title="Project One" description={description} tecnologies="HTML, CSS, JS"/>
-        <Project github="www.github.com" link="example.com" title="Project Two" description={description} tecnologies="Node, React"/>
+        <Project github="https://github.com/Adrianvc94/Movie-App" link="https://adrianvc94.github.io/Movie-App/" title="Movie List" description={MovieList_Description} tecnologies="HTML, CSS, JS" projectImg={movie_app}/>
+        <Project github="www.github.com" link="example.com" title="Project Two" description={description} tecnologies="Node, React" projectImg="https://images.unsplash.com/photo-1524985069026-dd778a71c7b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"/>
         {/* <Project github="www.github.com" link="example.com" title="Project Three" description={description} tecnologies="MongoDB, JS"/>
         
         <Project github="www.github.com" link="example.com" title="Project Four" description={description} tecnologies="Angular, .Net"/>
